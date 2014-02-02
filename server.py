7f0b5de936a6903c9eddcddc3262861fc441e652
Norton Wang <nortantivirus@gmail.com>
2014-02-02 16:14:37 -0500

    dos2unix, only return past week of temps

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,9 +8,29 @@ def index():
 
 @app.route('/temps.csv')
 def temps():
-    with open('temps.csv') as log_file:
-        temps = log_file.read()
-    return temps
+    with open('temps.csv') as f:
+        BUFSIZ = 1024
+        f.seek(0, 2)
+        bytes = f.tell()
+        size = 10800
+        block = -1
+        data = []
+        while size > 0 and bytes > 0:
+            if bytes - BUFSIZ > 0:
+                # Seek back one whole BUFSIZ
+                f.seek(block * BUFSIZ, 2)
+                # read BUFFER
+                data.insert(0, f.read(BUFSIZ))
+            else:
+                # file too small, start from begining
+                f.seek(0, 0)
+                # only read what was not read
+                data.insert(0, f.read(bytes))
+            linesFound = data[0].count('\n')
+            size -= linesFound
+            bytes -= BUFSIZ
+            block -= 1
+    return '\n'.join(''.join(data).splitlines()[-10800:])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
